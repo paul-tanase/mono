472ffbdb61a250d2ca330e820f9b7a33d044027d
Paul <paultanase2@gmail.com>
2025-01-07 21:46:41 -0500

    Update client

--- packages/client/index.js
+++ packages/client/index.js
@@ -1,7 +1,7 @@
 import hello from "../server/index.js";
 import _ from "lodash";
 
-console.log('Inside the client - update');
+console.log('Inside the client - update ...');
 hello();
 
 const check = _.includes([1, 2, 3], 1);
